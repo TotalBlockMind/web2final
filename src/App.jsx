6a38cb56489fb09be20edac430488154d2bba1c7
TotalBlockMind <joelorenzkiunisala@gmail.com>
2026-05-23 23:37:40 +0800

    Your message

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Landingpage from './components/Landingpage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/web2final/">
       <Routes>
         <Route path="/" element={<Landingpage />} />
       </Routes>
